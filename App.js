3ef50c23acb8946c0e45dbd5a33cbe23c338705a
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-05-15 10:26:03 -0400

    Add chat functionality with socket.io and fireworks effect

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// https://chat-app-server-kc0e.onrender.com
+
+import React, { useState, useEffect } from "react";
+import Fireworks from "react-native-fireworks";
+import {
+  SafeAreaView,
+  TextInput,
+  Button,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+import { io } from "socket.io-client";
+
+const socket = io("https://chat-app-server-kc0e.onrender.com");
 
 export default function App() {
+  const [message, setMessage] = useState("");
+  const [chatMessages, setChatMessages] = useState([]);
+  const [showFireworks, setShowFireworks] = useState(false);
+
+  useEffect(() => {
+    if (showFireworks) {
+      const time = setTimeout;
+    }
+  });
+
+  useEffect(() => {
+    socket.on("message", (msg) => {
+      setChatMessages((prev) => [...prev, msg]);
+    });
+    socket.on("lastMessages", (messages) => {
+      console.log(messages);
+      setChatMessages((prev) => [...prev, ...messages]);
+    });
+    console.log("emitting");
+    socket.emit("getLastMessages");
+    return () => {
+      socket.off("message");
+      socket.off("lastMessages");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (message.trim() !== "") {
+      // we will send to server
+      socket.emit("message", message);
+      setMessage("");
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={chatMessages}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <View style={styles.entryArea}>
+        <TextInput
+          style={styles.input}
+          value={message}
+          onChangeText={setMessage}
+        ></TextInput>
+        <Button title="Send" onPress={sendMessage} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgrey",
+    marginTop: 20,
+    marginBottom: 60,
+  },
+
+  entryArea: {
+    flexDirection: "row",
+    padding: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    flex: 1,
+    padding: 10,
+    marginRight: 10,
+    borderRadius: 5,
   },
 });
